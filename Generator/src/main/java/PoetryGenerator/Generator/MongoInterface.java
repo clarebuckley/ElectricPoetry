c0b0d55a417dd24c7810b3a68bd000a5d7fe331c
Clare Buckley <clarebuckley1@hotmail.com>
2018-11-08 13:19:49 +0000

    Finish mongodb interface

--- Generator/src/main/java/PoetryGenerator/Generator/MongoInterface.java
+++ Generator/src/main/java/PoetryGenerator/Generator/MongoInterface.java
@@ -1,27 +1,77 @@
 package PoetryGenerator.Generator;
 import com.mongodb.client.FindIterable; 
 import com.mongodb.client.MongoCollection; 
-import com.mongodb.client.MongoDatabase;  
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
 
 import java.util.Iterator; 
 import org.bson.Document; 
 import com.mongodb.MongoClient; 
 
-//TODO: use https://www.tutorialspoint.com/mongodb/mongodb_java.htm to finish class
-
+/**
+ * Interface for CRUD operations on MongoDB database
+ * Used in poetry generator and for testing purposes
+ * @author Clare Buckley
+ * @version 08/11/2018
+ *
+ */
 public class MongoInterface {
 
 	private MongoClient mongo = new MongoClient( "localhost" , 27017 ); 
 	private final MongoDatabase database;  
+	private final String databaseName;
 	
-	MongoInterface() {
-		database = mongo.getDatabase("poetrydb");
+	MongoInterface(String databaseNameParam) {
+		databaseName = databaseNameParam;
+		database = mongo.getDatabase(databaseName);
 	}
 	
+	/**
+	 * Get collection from database
+	 * @param collectionName
+	 * @return collection with name 'collectionName'
+	 */
 	public MongoCollection<Document> getCollection(String collectionName){
 		return database.getCollection(collectionName);
 	}
+		
+	/**
+	 * Insert a document into a collection
+	 * @param collectionName - collection to add document to
+	 * @param document - document to be added
+	 */
+	public void insertDocument(String collectionName, Document document) {
+		MongoCollection<Document> collection = getCollection(collectionName);
+		collection.insertOne(document);
+	}
 	
+	/**
+	 * Update document in a collection
+	 * @param collectionName - collection containing document
+	 * @param docId - document id
+	 * @param docKey - key for value to be updated
+	 * @param docVal - new value to update with
+	 */
+	public void updateDocument(String collectionName, int docId, String docKey, String docVal) {
+		MongoCollection<Document> collection = getCollection(collectionName);
+		collection.updateOne(Filters.eq("id", docId), Updates.set(docKey, docVal));
+	}
+	
+	/**
+	 * Delete document in a collection
+	 * @param collectionName - collection containing document
+	 * @param docId - id of document to be deleted
+	 */
+	public void deleteDocument(String collectionName, int docId) {
+		MongoCollection<Document> collection = getCollection(collectionName);
+		collection.deleteOne(Filters.eq("id", docId)); 
+	}
+	
+	/**
+	 * Print out collection contents
+	 * @param collectionName
+	 */
 	public void printCollectionContents(String collectionName) {
 		MongoCollection<Document> collection = getCollection(collectionName);
 		
@@ -34,5 +84,13 @@ public class MongoInterface {
 			count++; 
 		}
 	}
-
+	
+	/**
+	 * List all collections in the database
+	 */
+	public void listCollections() {
+		for(String name : database.listCollectionNames()) {
+			System.out.println(name);
+		}
+	}
 }
