3a4028d2a968c8749539a61626d109afe6ea9151
Clare Buckley <clarebuckley1@hotmail.com>
2018-11-13 14:32:53 +0000

    Start using Stanford NLP tagger

--- Generator/src/main/java/PoetryGenerator/Generator/PoemParser.java
+++ Generator/src/main/java/PoetryGenerator/Generator/PoemParser.java
@@ -1,10 +1,26 @@
 package PoetryGenerator.Generator;
 
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 
 public class PoemParser {
 
-	public static void main(String args[]) {
-		 
+	public static void main(String args[]) throws ClassNotFoundException, IOException {
+		new PoemParser();
+	}
+
+	public PoemParser() throws ClassNotFoundException, IOException {
+		MaxentTagger tagger = new MaxentTagger("taggers/bidirectional-distsim-wsj-0-18.tagger");
+
+		InputStream file = this.getClass().getResourceAsStream("/PoetryGenerator/Data/testFile.txt");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(file));
+		System.out.println(tagger.process(tagger.tokenizeText(reader)));
+
+		
+		//TODO The Stanford CoreNLP suite https://cloudacademy.com/blog/natural-language-processing-stanford-corenlp-2/
+		
 	}
 }
