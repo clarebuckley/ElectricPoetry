1cf45bb8fc027f3f91c1e20984f3776427f3a58b
Clare Buckley <clarebuckley1@hotmail.com>
2018-11-17 16:11:02 +0000

    Fix CoreNLP error

--- Generator/src/main/java/PoetryGenerator/Generator/PoemParser.java
+++ Generator/src/main/java/PoetryGenerator/Generator/PoemParser.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.List;
 
+import edu.stanford.nlp.simple.Sentence;
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 
 
@@ -15,11 +17,15 @@ public class PoemParser {
 	}
 
 	public PoemParser() throws ClassNotFoundException, IOException {
-		MaxentTagger tagger = new MaxentTagger("taggers/bidirectional-distsim-wsj-0-18.tagger");
+		MaxentTagger tagger = new MaxentTagger("taggers/english-bidirectional-distsim.tagger");
 
 		InputStream file = this.getClass().getResourceAsStream("/PoetryGenerator/Data/testFile.txt");
 		BufferedReader reader = new BufferedReader(new InputStreamReader(file));
 		System.out.println(tagger.process(tagger.tokenizeText(reader)));
+		
+		Sentence sent = new Sentence("Lucy is in the sky with diamonds.");
+		List<String> nerTags = sent.nerTags();  // [PERSON, O, O, O, O, O, O, O]
+		String firstPOSTag = sent.posTag(0);   // NNP
 
 		
 		//TODO The Stanford CoreNLP suite https://cloudacademy.com/blog/natural-language-processing-stanford-corenlp-2/
