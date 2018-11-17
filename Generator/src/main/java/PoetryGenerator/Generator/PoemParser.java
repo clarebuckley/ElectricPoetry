02eb067c26a6d866461762fd259d6ac272be9334
Clare Buckley <clarebuckley1@hotmail.com>
2018-11-17 16:54:51 +0000

    Get words, pos tags, ner tags, map of chain, tree and dependencies for each line

--- Generator/src/main/java/PoetryGenerator/Generator/PoemParser.java
+++ Generator/src/main/java/PoetryGenerator/Generator/PoemParser.java
@@ -1,15 +1,16 @@
 package PoetryGenerator.Generator;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
+import edu.stanford.nlp.coref.CorefCoreAnnotations.CorefChainAnnotation;
+import edu.stanford.nlp.coref.data.CorefChain;
 import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
@@ -18,8 +19,10 @@ import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.simple.Sentence;
-import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+import edu.stanford.nlp.semgraph.SemanticGraph;
+import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.CollapsedDependenciesAnnotation;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
 import edu.stanford.nlp.util.CoreMap;
 
 
@@ -30,19 +33,15 @@ public class PoemParser {
 	}
 
 	public PoemParser() throws ClassNotFoundException, IOException {
-		MaxentTagger tagger = new MaxentTagger("taggers/english-bidirectional-distsim.tagger");
-
 		InputStream file = this.getClass().getResourceAsStream("/PoetryGenerator/Data/testFile.txt");
 		BufferedReader reader = new BufferedReader(new InputStreamReader(file));
 		String line = reader.readLine();
 
 		while (line != null) {
-			// creates a StanfordCoreNLP object, with POS tagging, lemmatization,
-			// NER, parsing, and coreference resolution
+			// creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER, parsing, and coreference resolution
 			Properties props = new Properties();
 			props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
 			StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-
 
 			// create an empty Annotation just with the given text
 			Annotation document = new Annotation(line);
@@ -51,35 +50,48 @@ public class PoemParser {
 			pipeline.annotate(document);
 
 			// these are all the sentences in this document
-			List<CoreMap> sentences = document.get(SentencesAnnotation.class);
-
+			List <CoreMap> sentences = document.get(SentencesAnnotation.class);
 			List<String> words = new ArrayList<String>();
 			List<String> posTags = new ArrayList<String>();
 			List<String> nerTags = new ArrayList<String>();
+
 			for (CoreMap sentence : sentences) {
 				// traversing the words in the current sentence
 				for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
-					// this is the text of the token
+					//text of the token
 					String word = token.get(TextAnnotation.class);
 					words.add(word);
-					// this is the POS tag of the token
+
+					//POS tag of the token
 					String pos = token.get(PartOfSpeechAnnotation.class);
 					posTags.add(pos);
-					// this is the NER label of the token
+
+					//NER label of the token
 					String ne = token.get(NamedEntityTagAnnotation.class);
 					nerTags.add(ne);
 				}
+
+				//Syntactic parse tree of sentence 
+				Tree tree = sentence.get(TreeAnnotation.class); 
+				System.out.println("Tree:\n"+ tree); 
+
+				//Dependency graph of the sentence 
+				SemanticGraph dependencies = sentence.get(CollapsedDependenciesAnnotation.class); 
+				System.out.println("Dependencies\n:"+ dependencies);
 			}
-			System.out.println(words.toString());
-			System.out.println(posTags.toString());
-			System.out.println(nerTags.toString());
-			
+
+			System.out.println("Words: " + words.toString());
+			System.out.println("posTags: " + posTags.toString());
+			System.out.println("nerTags: " + nerTags.toString());
+
+			//Map of the chain 
+			Map<Integer, CorefChain> graph = document.get(CorefChainAnnotation.class); 
+			System.out.println("Map of the chain:\n" + graph);
+
+
 			// read next line
 			line = reader.readLine();
 		}
-
-
-
 
 	}
 
