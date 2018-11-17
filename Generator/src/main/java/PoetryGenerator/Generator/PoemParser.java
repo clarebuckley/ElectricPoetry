5caf3f05cae164defe24e908f091c23a28355a85
Clare Buckley <clarebuckley1@hotmail.com>
2018-11-17 16:38:06 +0000

    PoemParser successfully reads and parses each line from a single poem in a test file contained in different java package

--- Generator/src/main/java/PoetryGenerator/Generator/PoemParser.java
+++ Generator/src/main/java/PoetryGenerator/Generator/PoemParser.java
@@ -1,13 +1,26 @@
 package PoetryGenerator.Generator;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
+import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.simple.Sentence;
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+import edu.stanford.nlp.util.CoreMap;
 
 
 public class PoemParser {
@@ -21,14 +34,54 @@ public class PoemParser {
 
 		InputStream file = this.getClass().getResourceAsStream("/PoetryGenerator/Data/testFile.txt");
 		BufferedReader reader = new BufferedReader(new InputStreamReader(file));
-		System.out.println(tagger.process(tagger.tokenizeText(reader)));
-		
-		Sentence sent = new Sentence("Lucy is in the sky with diamonds.");
-		List<String> nerTags = sent.nerTags();  // [PERSON, O, O, O, O, O, O, O]
-		String firstPOSTag = sent.posTag(0);   // NNP
+		String line = reader.readLine();
 
-		
-		//TODO The Stanford CoreNLP suite https://cloudacademy.com/blog/natural-language-processing-stanford-corenlp-2/
-		
+		while (line != null) {
+			// creates a StanfordCoreNLP object, with POS tagging, lemmatization,
+			// NER, parsing, and coreference resolution
+			Properties props = new Properties();
+			props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+			StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+
+			// create an empty Annotation just with the given text
+			Annotation document = new Annotation(line);
+
+			// run all Annotators on this text
+			pipeline.annotate(document);
+
+			// these are all the sentences in this document
+			List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+
+			List<String> words = new ArrayList<String>();
+			List<String> posTags = new ArrayList<String>();
+			List<String> nerTags = new ArrayList<String>();
+			for (CoreMap sentence : sentences) {
+				// traversing the words in the current sentence
+				for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
+					// this is the text of the token
+					String word = token.get(TextAnnotation.class);
+					words.add(word);
+					// this is the POS tag of the token
+					String pos = token.get(PartOfSpeechAnnotation.class);
+					posTags.add(pos);
+					// this is the NER label of the token
+					String ne = token.get(NamedEntityTagAnnotation.class);
+					nerTags.add(ne);
+				}
+			}
+			System.out.println(words.toString());
+			System.out.println(posTags.toString());
+			System.out.println(nerTags.toString());
+			
+			// read next line
+			line = reader.readLine();
+		}
+
+
+
+
 	}
+
+
 }
