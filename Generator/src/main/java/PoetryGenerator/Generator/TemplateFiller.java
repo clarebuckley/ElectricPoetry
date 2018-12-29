22d8d893a6f9585158a678d1e9b59e4a794aeb82
Clare Buckley <clarebuckley1@hotmail.com>
2018-12-29 13:06:16 +0000

    Add missing punctuation

--- Generator/src/main/java/PoetryGenerator/Generator/TemplateFiller.java
+++ Generator/src/main/java/PoetryGenerator/Generator/TemplateFiller.java
@@ -30,23 +30,27 @@ public class TemplateFiller {
 		Random random = new Random();
 		String line = "";
 		for(int i = 0; i < tags.length; i++) {	
-			String punctuation = ".,:;";
+			String punctuation = ".,:;``";
 
+			//Add punctuation
 			if(punctuation.contains(tags[i])) {
+				//Remove space before punctuation
 				line = line.substring(0, line.length()-1);
 				line += tags[i];
 			} else {
+				//Replace tags with words
 				ArrayList<String> words = (ArrayList<String>) mongo.getTagWords("wordbank", tags[i]);
 				int numOfWords = words.size();
 				int randomIndex = random.nextInt(numOfWords);	
 				line += words.get(randomIndex);	
 			} 
 
+			//Don't add space after word if end of line
 			if(i != tags.length-1) {
 				line += " ";
 			}
 
-
+			//Set start of lines to have capital letters
 			line = line.substring(0, 1).toUpperCase() + line.substring(1);
 
 		}
